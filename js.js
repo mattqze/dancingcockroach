4345ff99c62fa034e3eb22cc26011cd7cb0b279b
mattqze <96447310+mattqze@users.noreply.github.com>
2022-06-28 18:02:37 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+function copyfunc(){
+    navigator.clipboard.writeText("dancingcockroach.netlify.app");
+    var copiedtext = document.getElementById("copytxt")
+    copiedtext.style.display = "block";
+}
